ceb2a15f62ddafbd0c3639ea21d26e58b880e6f9
YaCZHong <1847635230@qq.com>
2021-08-26 19:44:07 +0800

    [*]完善部件;

---
+++ app/src/main/java/com/czh/yuji_widget/appwidget/Constants.kt
@@ -0,0 +1,3 @@
+package com.czh.yuji_widget.appwidget
+
+const val ACTION_UPDATE_WIDGET = "com.czh.yuji_widget.action.APPWIDGET_UPDATE"
--- app/src/main/java/com/czh/yuji_widget/appwidget/SimpleAppWidgetProvider.kt
+++ app/src/main/java/com/czh/yuji_widget/appwidget/SimpleAppWidgetProvider.kt
@@ -1,7 +1,31 @@
 package com.czh.yuji_widget.appwidget
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.czh.yuji_widget.MainActivity
+import com.czh.yuji_widget.R
 
 class SimpleAppWidgetProvider : AppWidgetProvider() {
-
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        appWidgetIds.forEach { appWidgetId ->
+            val pendingIntent = Intent(context, MainActivity::class.java).let { intent ->
+                PendingIntent.getActivity(context, 0, intent, 0)
+            }
+            val remoteViews = RemoteViews(context.packageName, R.layout.appwidget_simple).apply {
+                setOnClickPendingIntent(R.id.tv_jump, pendingIntent)
+                setImageViewResource(R.id.iv_weather, R.drawable.ic_weather_duoyun)
+                setTextViewText(R.id.tv_temp, "29℃")
+                setTextViewText(R.id.tv_city, "天河区")
+            }
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+        }
+    }
 }
